6353a2dbed9690830bf13d2ebaf9224547fa687e
HAL10001\adria <adrianyorke@gmail.com>
2017-07-13 02:37:05 +0300

    Updated main.py - added INFO links and other comments

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+# INFO: Sample files - https://www.w3schools.com/xml/schema_example.asp
+# INFO: https://stackoverflow.com/questions/2349991/python-how-to-import-other-python-files
+# INFO: https://stackoverflow.com/questions/5788891/execute-a-file-with-arguments-in-python-shell
+
+# Modified XSD file to make it fail: renamed element title to title_fail
+
 print 'Starting this beautiful thing...'
 
 sys.argv = ['XSDValidator.py','shiporder.xml','shiporder.xsd']
