a10cf81b74db6caf6477741d27268d39e1e106ad
HAL10001\adria <adrianyorke@gmail.com>
2017-08-18 00:24:29 +0300

    Minor refactoring based on lint feedback

--- src/python/PythonUnitTest.py
+++ src/python/PythonUnitTest.py
@@ -7,7 +7,7 @@ import sys
 import unittest
 
 class TestStringMethods(unittest.TestCase):
-
+    """Class docstring [pylint-C0111]."""
     def test_always_pass(self):
         """Test Case: Always Pass"""
         pass
@@ -28,11 +28,11 @@ class TestStringMethods(unittest.TestCase):
 
     def test_split(self):
         """Test Case: split()"""
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
+        mystr = 'hello world'
+        self.assertEqual(mystr.split(), ['hello', 'world'])
+        # check that mystr.split fails when the separator is not a string
         with self.assertRaises(TypeError):
-            s.split(2)
+            mystr.split(2)
 
 if __name__ == '__main__':
     print(sys.version_info)
