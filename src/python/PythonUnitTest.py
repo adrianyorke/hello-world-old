fc1590567ad74119aea357e32aca56b85e97c1bf
HAL10001\adria <adrianyorke@gmail.com>
2017-08-18 00:09:06 +0300

    More updates

--- src/python/PythonUnitTest.py
+++ src/python/PythonUnitTest.py
@@ -3,24 +3,31 @@
 
 from __future__ import print_function
 
+import sys
 import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-    def testAlwaysPass(self):
+    def test_always_pass(self):
+        """Test Case: Always Pass"""
         pass
 
-    def testAlwaysFail(self):
+    @unittest.skip("demonstrating skipping - failure is ignored")
+    def test_always_fail(self):
+        """Test Case: Always Fail"""
         self.fail('This is supposed to always fail!')
 
     def test_upper(self):
+        """Test Case: upper()"""
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
+        """Test Case: isupper()"""
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
     def test_split(self):
+        """Test Case: split()"""
         s = 'hello world'
         self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
@@ -28,4 +35,5 @@ class TestStringMethods(unittest.TestCase):
             s.split(2)
 
 if __name__ == '__main__':
+    print(sys.version_info)
     unittest.main()
